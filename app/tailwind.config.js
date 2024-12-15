f47e5a2d5388371040eb7f43fa7ed7f7b71be1c9
Shantanu-code1 <shantanu@eazybe.com>
2024-12-16 01:09:31 +0530

    feat: npm i

---
+++ app/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add your React files here
+  ],
+  theme: {
+    extend: {}, // Customize your theme here
+  },
+  plugins: [], // Add plugins if needed
+};
